71b8e339a92549dbcb76d68bd78d7b777f6e7895
Akhil <akhilsanthosh480@gmail.com>
2026-07-02 00:52:35 +0530

    improved mac for 2x2 array

--- sysaray2x2.v
+++ sysaray2x2.v
@@ -1,30 +1,38 @@
 module
     mac (
         input clk,
-        input  a_in, 
-        input b_in,
+        input rst_n,    // _n: negative logic
+        input  [7:0] a_in,     // int8
+        input [7:0] b_in,
         input valid,
         input clear,
-        output aout,
-        output bout,
-        output reg acc
+        output reg[7:0] a_out,
+        output reg[7:0]b_out,
+        output reg[20:0] acc
     );
     
     
-    always@(posedge clk )
+  always@(posedge clk or negedge rst_n)
+  begin
+    if(!rst_n)
     begin
-        if(clear)
-            begin
-                acc <= 0;
-                aout<= 0;
-                bout <= 0;
-            end
-         else if(valid)
-        begin
-            acc <= acc + (a_in*b_in);
-            aout <= a_in;
-            bout <= b_in;
-        end
-                   
+      a_out<= 8'h00;
+      b_out<= 8'h00;
+      acc<= 8'h00;
     end
+    else
+    begin
+      a_out<=a_in;
+      b_out<=b_in;
+      if(clear)
+      begin
+        acc<=0;
+      end
+      else if (valid) 
+      begin
+      acc<=acc +(a_in*b_in);  
+      end
+    end
+  end
+
 endmodule
