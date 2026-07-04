63d6a763ff461a462786172afae240b552b51296
Akhil <akhilsanthosh480@gmail.com>
2026-07-04 17:49:05 +0530

    reworked systolic array module and readme uodated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Systolic Array
 
+## Overview
+
 Systolic array is a grid of Multiply-Accumulate(MAC) units and is used for efficient matrix multiplication and other linear algebra applications . These are used in CNN were better speed for matirx multiplication matters.
 
 First a mac unit which simply does the job of acc<=acc +(a_in*b_in) was implemented.
@@ -35,4 +37,20 @@ The purpose of clear is not to flush the pipeline but  it is to reset the accumu
 Now if there is a valid signal, then the accum_out action happens, multiplication is done and the running sum in the accumulator register is updated. Also in the multiplier part, the width of a_in*b_in is extended to ACCUM_WIDTH to remove the ambiguity.
 
 
+# Systolic array
 
+## Module overview
+
+Systolic array module instatiates ARRAY_SIZE × ARRAY_SIZE mac_units and the design was generalized using parameters for array size and data width.These are interconnected by the busses(a_bus and b_bus)which again are parameterised and doesnt need any mannual wiring. There are seperate  generate blocks maintained for:
+             -Left-edge assignments (a_in → a_bus)
+             -Top-edge assignments (b_in → b_bus)
+             -Instantiation of the mac_unit array
+
+## Module functioning
+
+mac_units are initiated and the parameters are passed explicitly for avoiding any ambiguity during future scaling. The units will be instatioated as :
+                       gen_rows[i]gen_coloums[j].[pe]
+
+The wiring is done in such a way that the edge assignments are done explicitly and if it is not an edge mac_unit, then a fill be fed from the left edge and b will be fed form the top edge.
+
+Each MAC performs the mac operation whenever the valid signal is asserted. The accumulated output (accum_out) of every processing element is directly connected to the corresponding element of the results matrix, where results[i][j] represents the accumulated output of the processing element located at row i and column j
--- systolicarray.sv
+++ systolicarray.sv
@@ -5,8 +5,8 @@ parameter ACCUM_WIDTH = 21
 )(
 input logic clk,
 input logic rst_n,
-input logic [DATA_WIDTH-1:0] a_in [ARRAY_SIZE-1:0],
-input logic [DATA_WIDTH-1:0] b_in [ARRAY_SIZE-1:0],
+input logic signed [DATA_WIDTH-1:0] a_in [ARRAY_SIZE-1:0],
+input logic signed [DATA_WIDTH-1:0] b_in [ARRAY_SIZE-1:0],
 input logic valid,
 input logic clear,
 output logic signed [ACCUM_WIDTH-1:0]
@@ -16,8 +16,8 @@ results [ARRAY_SIZE-1:0][ARRAY_SIZE-1:0]
 
 //interconnection wires
 
- logic [(DATA_WIDTH-1):0]a_bus[(ARRAY_SIZE-1):0][(ARRAY_SIZE):0];
- logic [(DATA_WIDTH-1):0]b_bus[(ARRAY_SIZE):0][(ARRAY_SIZE-1):0];
+ logic signed [(DATA_WIDTH-1):0]a_bus[(ARRAY_SIZE-1):0][(ARRAY_SIZE):0];
+ logic signed [(DATA_WIDTH-1):0]b_bus[(ARRAY_SIZE):0][(ARRAY_SIZE-1):0];
 
 
 // a_bus[0][0] ----> PE00 ----> a_bus[0][1] ----> PE01 ----> a_bus[0][2]
@@ -26,25 +26,43 @@ results [ARRAY_SIZE-1:0][ARRAY_SIZE-1:0]
 
 
  // pe matrix
-genvar i,j;
+
+//Left end assignments
 generate
-    for (i=0; i<(ARRAY_SIZE); i++)
+    for (genvar i=0; i<(ARRAY_SIZE) ; i++)
     begin
-        for (j=0; j<(ARRAY_SIZE); j++)
-        begin
+       assign a_bus[i][0]= a_in[i];  
+    end
+endgenerate
+
+//Top edge assignments
+generate
+    for (genvar j=0; j<(ARRAY_SIZE) ; j++)
+    begin
+        assign b_bus[0][j]=b_in[j]; 
+    end
+endgenerate
+
+ //Mac instantiations
+generate
+    for (genvar i=0; i<(ARRAY_SIZE); i++)
+    begin : gen_rows
+        for (genvar j=0; j<(ARRAY_SIZE); j++)
+        begin :gen_columns
             // if j=0, accept a from a_inor else accept from left 
             // if i=0, accept input from b_in or else from above
-            mac_unit pe(.clk(clk), .rst_n(rst_n), .valid(valid), .clear(clear),
+            mac_unit #(.DATA_WIDTH(DATA_WIDTH),.ACCUM_WIDTH(ACCUM_WIDTH))
+            pe(.clk(clk), .rst_n(rst_n), .valid(valid), .clear(clear),
             .a_in(a_bus[i][j]), .b_in(b_bus[i][j]), 
             .a_out(a_bus[i][j+1]), .b_out(b_bus[i+1][j]),
-            .acc(results[i][j]));
+            .accum_out(results[i][j]));
         end
        
     end
 endgenerate
 
 
-// mac pe(.clk(clk), .rst_n(rst_n), .valid(valid), .clear(clear),
+// mac_unit pe(.clk(clk), .rst_n(rst_n), .valid(valid), .clear(clear),
 //     .a_in(a_in[i]), .b_in(b_in[i]), )
 
 //  mac (
